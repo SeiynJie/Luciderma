8488089b4ba1848a60e5ee548de991e52e3ad150
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-03-25 16:51:43 +0800

    Home/TopDoctors - Added Component
    Changes:
    - Added component, uses data from `assets.js` export

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { doctors } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const TopDoctors = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="my-16 flex flex-col items-center gap-4 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-medium">Top Doctors</h1>
+      <p className="text-center text-sm sm:w-1/3">
+        Simply browse through our extensive list of trusted doctors.
+      </p>
+
+      <div className="grid w-full grid-cols-5 gap-4 gap-y-6 px-3 pt-5 sm:px-0">
+        {doctors.slice(0, 10).map((item, index) => {
+          return (
+            <div
+              onClick={() => navigate(`/appointment/${item._id}`)}
+              key={index}
+              className="cursor-pointer overflow-hidden rounded-xl border border-blue-200 transition-all duration-500 hover:translate-y-[-10px]"
+            >
+              <img className="bg-blue-50" src={item.image} alt="" />
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-center text-sm text-green-500">
+                  <p className="size-2 rounded-full bg-green-500"></p>
+                  <p>Available</p>
+                </div>
+                <p className="text-lg font-medium text-gray-900">{item.name}</p>
+                <p className="text-sm text-gray-600">{item.speciality}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <button className="mt-10 rounded-full bg-blue-50 px-12 py-3 text-gray-600">
+        More
+      </button>
+    </div>
+  );
+};
+
+export default TopDoctors;
