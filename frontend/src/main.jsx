89839a3a8ca20ec949a3f98ad3800ef5e8e10a80
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-03-25 13:11:56 +0800

    (Template Checkpoint) Pages Setup
    Changes:
    - Setup the pages with react router dom
    - Can be used as template

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// ? For routing
+import { BrowserRouter } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
