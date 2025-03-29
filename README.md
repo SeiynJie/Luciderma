c9b0443653c9151f800b2a89deac0daf44bb45e1
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-03-29 12:43:20 +0800

    Backend/Cloudinary - Added connection
    Changes:
    - Added connectCloudinary function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Luciderma
  A derma appointment/booking system built for a local derma company. 
+
+# Powered by:
+- Vite + React 
+- Tailwind
+- Mongo DB
+- Cloudinary
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
-import express from 'express'
-import cors from 'cors'
-import "dotenv/config"
-import connectDB from './config/mongodb.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // App config
-const app = express()
-const port = process.env.PORT || 4000 // Port from .env OR port 4000
-connectDB()
+const app = express();
+const port = process.env.PORT || 4000; // Port from .env OR port 4000
+connectDB();
+connectCloudinary();
 
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-// API Endpoints 
-app.get("/", (request, response)=>{
-    response.send("API working Greatsz")
-})
+// API Endpoints
+app.get("/", (request, response) => {
+  response.send("API working Greatsz");
+});
 
 // Start express app
-app.listen(port, ()=>console.log("Server Started", port))
+app.listen(port, () => console.log("Server Started", port));
