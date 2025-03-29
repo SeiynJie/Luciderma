034b4a2a7dda616e9b605e99f73dfc83487954e8
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-03-29 23:15:07 +0800

    Admin/Login - Added logout
    Changes:
    - Added ability to logout

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { AdminContext } from "../context/AdminContext";
+
+const Navbar = () => {
+  const { aToken, setAToken } = useContext(AdminContext);
+
+  // Function for logout
+  const logout = (params) => {
+    aToken && setAToken("")
+    aToken && localStorage.removeItem("aToken")
+  }
+
+  return (
+    <div className="flex justify-between items-center px-4 sm:px-10 border-b bg-white border-gray-200">
+      <div className="flex items-center gap-2 text-xs">
+        <img className="w-36 sm:w-40 cursor-pointer" src={assets.admin_logo} alt="admin logo" />
+        <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">{aToken ? `Admin` : "Doctor"}</p>
+      </div>
+
+      <button onClick={logout} className="bg-primary text-white text-sm px-10 py-2 rounded-full">Logout</button>
+    </div>
+  );
+};
+
+export default Navbar;
