87216a304c09a24eb60e6fe2843840acd9178eb5
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-03-29 23:16:52 +0800

    Admin/Login - Navigate on Logout
    Changes:
    - Added the navigate when doing logout
    
    - formatted file

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,24 +1,39 @@
 import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { AdminContext } from "../context/AdminContext";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { aToken, setAToken } = useContext(AdminContext);
 
+  const navigate = useNavigate();
+
   // Function for logout
   const logout = (params) => {
-    aToken && setAToken("")
-    aToken && localStorage.removeItem("aToken")
-  }
+    navigate("/");
+    aToken && setAToken("");
+    aToken && localStorage.removeItem("aToken");
+  };
 
   return (
     <div className="flex justify-between items-center px-4 sm:px-10 border-b bg-white border-gray-200">
       <div className="flex items-center gap-2 text-xs">
-        <img className="w-36 sm:w-40 cursor-pointer" src={assets.admin_logo} alt="admin logo" />
-        <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">{aToken ? `Admin` : "Doctor"}</p>
+        <img
+          className="w-36 sm:w-40 cursor-pointer"
+          src={assets.admin_logo}
+          alt="admin logo"
+        />
+        <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
+          {aToken ? `Admin` : "Doctor"}
+        </p>
       </div>
 
-      <button onClick={logout} className="bg-primary text-white text-sm px-10 py-2 rounded-full">Logout</button>
+      <button
+        onClick={logout}
+        className="bg-primary text-white text-sm px-10 py-2 rounded-full"
+      >
+        Logout
+      </button>
     </div>
   );
 };
