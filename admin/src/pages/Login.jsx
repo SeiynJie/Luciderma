98711871374d7a8bb51475686e2ddd1ffef4d22c
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-03-29 22:18:18 +0800

    Admin/Login - Initial login API logic
    Changes:
    - Added the initial code for logging in admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,31 +1,96 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { assets } from "../assets/assets";
+import { AdminContext } from "../context/AdminContext";
+import axios from "axios";
 
 const Login = () => {
   const [state, setState] = useState("Admin");
 
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
+  // Store email ID
+  const [email, setEmail] = useState("")
+  
+  // Store password
+  const [password, setPassword] = useState("")
+
+  // Function for submitting form
+  const onSubmitHandler = async (event) => {
+    event.preventDefault() // Won't reload webpage when form submitted
+
+    try {
+        // API call
+        if (state === "Admin") {
+            // Admin Login API
+            const {data} = await axios.post(backendUrl + "/api/admin/login", {email, password})
+
+            if (data.success) {
+                // console.log(data.token)
+                // Also save to local storage
+                localStorage.setItem("aToken", data.token)
+                
+                setAToken(data.token)
+            }
+        } else {
+            // Doctor login API
+
+        }
+    } catch (error) {
+        
+    }
+  }
+
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form className="min-h-[80vh] flex items-center" onSubmit={onSubmitHandler}>
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border border-gray-100 rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl font-semibold m-auto">
           <span className="text-primary">{state}</span> Login
         </p>
         <div className="w-full">
           <p>Email</p>
-          <input className="border border-[#DADADA] rounded w-full p-2 mt-1" type="email" required />
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="email"
+            required
+            onChange={(e)=>setEmail(e.target.value)}
+            value={email}
+            />
         </div>
         <div className="w-full">
           <p>Password</p>
-          <input className="border border-[#DADADA] rounded w-full p-2 mt-1" type="password" required />
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="password"
+            required
+            onChange={(e)=>setPassword(e.target.value)}
+            value={password}
+          />
         </div>
 
-        <button className="bg-primary text-white w-full py-2 rounded-md text-base">Login</button>
-        {
-            state === "Admin" ? 
-            <p>Doctor Login? <span onClick={()=> setState("Doctor")} className="cursor-pointer text-primary underline">Click here</span></p>
-            :
-            <p>Admin Login? <span onClick={()=> setState("Admin")} className="cursor-pointer text-primary underline">Click here</span></p>
-        }
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          Login
+        </button>
+        {state === "Admin" ? (
+          <p>
+            Doctor Login?{" "}
+            <span
+              onClick={() => setState("Doctor")}
+              className="cursor-pointer text-primary underline"
+            >
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login?{" "}
+            <span
+              onClick={() => setState("Admin")}
+              className="cursor-pointer text-primary underline"
+            >
+              Click here
+            </span>
+          </p>
+        )}
       </div>
     </form>
   );
