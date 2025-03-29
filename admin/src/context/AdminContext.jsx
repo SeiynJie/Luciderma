b8276efdd7191f90b8a6117240a6604cd3e4ba37
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-03-29 22:06:05 +0800

    Admin/AdminContext - Added values and backendurl
    Changes:
    `.env`
    - created file on root folder and added `VITE_BACKEND_URL="http://localhost:4000"`
    
    `AdminContext.jsx`
    - Initialized states and values with backend url to be accessible to all components

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,15 +1,22 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AdminContext = createContext();
 
 // ? Context file that will update everything. Acts as a modular storage that also runs events on update
 const AdminContextProvider = (props) => {
+  const [aToken, setAToken] = useState("");
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const value = {
-    
+    aToken,
+    setAToken,
+    backendUrl,
   };
 
   return (
-    <AdminContext.Provider value={value}>{props.children}</AdminContext.Provider>
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
   );
 };
 
