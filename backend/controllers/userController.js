71da0b159c9e88fc052d4e8ff3b0acffe5d52761
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-03-31 19:49:36 +0800

    Backend/UserLogin - User Registration
    Changes:
    `userController.js`
    - Created  controller with function to register the user and save in database
    
    `userRouter.js`
    - Created API endpoint
    
    `server.js`
    - Will use newly created user endpoint

---
+++ backend/controllers/userController.js
@@ -0,0 +1,60 @@
+// API logic for the user (login, booking, etc.)
+
+import validator from "validator";
+import bcrypt from "bcrypt";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+//* User Registration API
+const registerUser = async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    // Check if any is invalid or empty
+    if (!name || !email || !password) {
+      return response.json({
+        success: false,
+        message: "Missing/Invalid Details",
+      });
+    }
+
+    // Validate email
+    if (!validator.isEmail(email)) {
+      return response.json({ success: false, message: "Enter a Valid Email" });
+    }
+
+    // Validate password
+    if (password.length < 8) {
+      return response.json({
+        success: false,
+        message: "Password must be 8 characters or more",
+      });
+    }
+
+    //* Add user in DB
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    //* Save to DB
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    // Create token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    response.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    response.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser };
