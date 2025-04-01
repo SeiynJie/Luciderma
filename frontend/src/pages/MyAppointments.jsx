373932deff003ede701daca674b366fc64a38d2e
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-04-01 16:01:23 +0800

    Backend/MyAppointments - Retrieve Appointments
    `MyAppointments.jsx`
    - Will use the appointments array instead of doctors array in the `.slice`
    - Created function to get the user's appointments (will call API)
    
    - Updated the sources to get from the `docData` obj stored in the objects that was returned by the API call
    - Updated styling a bit

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,31 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const MyAppointments = () => {
-  const { doctors } = useContext(AppContext);
+  const { backendUrl, token } = useContext(AppContext);
+  const [appointments, setAppointments] = useState([]);
 
+  const getUserAppointments = async () => {
+    try {
+      // Call API
+      const { data } = await axios.get(backendUrl + "/api/user/appointments", {
+        headers: { token },
+      });
+
+      if (data.success) {
+        // reverse the array then Set the state because the most recent is at the bottom of the array
+        const reversedAppointments = [...data.appointments].reverse();
+        setAppointments(reversedAppointments);
+        console.log(data.appointments);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      getUserAppointments();
+    }
+  }, [token]);
   return (
     <div>
-      <p className="mt-12 border-b pb-3 font-medium text-zinc-700">
+      <p className="mt-12 border-b border-gray-200 pb-3 font-medium text-zinc-700">
         My Appointments
       </p>
       <div>
-        {doctors.slice(0, 3).map((item, index) => {
+        {appointments.slice(0, 3).map((item, index) => {
           return (
             <div
               key={index}
-              className="grid-cols-[1fr_2fr] grid gap-4 border-b py-2 sm:flex sm:gap-6"
+              className="grid grid-cols-[1fr_2fr] gap-4 border-b border-gray-200 py-2 sm:flex sm:gap-6"
             >
               <div>
-                <img src={item.image} alt="" className="w-32 bg-indigo-50" />
+                <img
+                  src={item.docData.image}
+                  alt="doctor data"
+                  className="w-32 rounded-2xl bg-indigo-50"
+                />
               </div>
 
               <div className="flex-1 text-sm text-zinc-600">
-                <p className="font-semibold text-neutral-800">{item.name}</p>
-                <p>{item.speciality}</p>
+                <p className="font-semibold text-neutral-800">
+                  {item.docData.name}
+                </p>
+                <p>{item.docData.speciality}</p>
                 <p className="mt-1 font-medium text-zinc-700">Address</p>
-                <p className="text-xs">{item.address.line1}</p>
-                <p className="text-xs">{item.address.line2}</p>
+                <p className="text-xs">{item.docData.address.line1}</p>
+                <p className="text-xs">{item.docData.address.line2}</p>
                 <p className="mt-1 text-sm">
                   <span className="text-sm font-medium text-neutral-700">
                     Date & Time:{" "}
@@ -38,10 +71,10 @@ const MyAppointments = () => {
               <div></div>
 
               <div className="flex flex-col justify-end gap-2">
-                <button className="hover:bg-primary rounded border py-2 text-center text-sm text-stone-500 transition-all duration-300 hover:text-white sm:min-w-48">
+                <button className="hover:bg-primary rounded border border-gray-200 py-2 text-center text-sm text-stone-500 transition-all duration-300 hover:text-white sm:min-w-48">
                   Pay Online
                 </button>
-                <button className="rounded border py-2 text-center text-sm text-stone-500 transition-all duration-300 hover:bg-red-600 hover:text-white sm:min-w-48">
+                <button className="rounded border border-gray-200 py-2 text-center text-sm text-stone-500 transition-all duration-300 hover:bg-red-600 hover:text-white sm:min-w-48">
                   Cancel Appointment
                 </button>
               </div>
