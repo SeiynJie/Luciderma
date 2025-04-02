aafd6bab982d27e54406bf43c52f31644bec2404
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-04-02 19:39:18 +0800

    Backend/Doc - Doctor Login
    `doctorController.js`
    - Created function for logging in doctor
    
    `doctorRoute.js`
    - Created API endpoint for logging doctor in

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,6 +1,8 @@
 // Needed for both the admin and the normal doctor user so I added it here instead of just adminController.jsx
 
 import doctorModel from "../models/doctorModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 //* Change the doctors availability
 const changeAvailability = async (request, response) => {
@@ -30,4 +32,36 @@ const doctorList = async (request, response) => {
     response.json({ success: false, message: error });
   }
 };
-export { changeAvailability, doctorList };
+
+//* Doctor Login API
+const loginDoctor = async (request, response) => {
+  try {
+    // Get email and password from user's request
+    const { email, password } = request.body;
+
+    const doctor = await doctorModel.findOne({ email });
+
+    // Check if doctor exists with the email ID
+    if (!doctor) {
+      return response.json({
+        success: false,
+        message: "Doctor does not exist",
+      });
+    }
+
+    // Match password
+    const isMatch = await bcrypt.compare(password, doctor.password);
+
+    if (isMatch) {
+      // Send token to user
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      response.json({ success: true, token });
+    } else {
+      response.json({ success: false, message: "Incorrect Password" });
+    }
+  } catch (error) {
+    console.log(error);
+    response.json({ success: false, message: error });
+  }
+};
+export { changeAvailability, doctorList, loginDoctor };
