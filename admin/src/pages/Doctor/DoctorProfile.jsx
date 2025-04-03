1f6b36fb68b69191d199e4c223732646bc28d808
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-04-03 16:16:25 +0800

    Admin/DocProf - Update Doc Prof
    `DoctorProfile.jsx`
    - Added edit mode UI

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { DoctorContext } from "../../context/DoctorContext";
 import { AppContext } from "../../context/AppContext";
 
@@ -8,6 +8,7 @@ const DoctorProfile = () => {
 
   const { formatDate, currencySymbol, backendUrl } = useContext(AppContext);
 
+  const [isEdit, setIsEdit] = useState(false);
   useEffect(() => {
     if (dToken) {
       getProfileData();
@@ -54,25 +55,95 @@ const DoctorProfile = () => {
               Appointment Fee:{" "}
               <span className="text-gray-800">
                 {currencySymbol}
-                {profileData.fees}
+                {isEdit ? (
+                  <input
+                    className="bg-gray-100 rounded-xl max-w-20 pl-5"
+                    value={profileData.fees}
+                    type="number"
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        fees: e.target.value,
+                      }))
+                    }
+                  />
+                ) : (
+                  profileData.fees
+                )}
               </span>
             </p>
 
             <div className="flex gap-2 py-2">
               <p>Address:</p>
               <p className="text-sm mt-1">
-                {profileData.address.line1} <br /> {profileData.address.line2}
+                {isEdit ? (
+                  <input
+                    className="bg-gray-100 rounded-xl max-w-fit px-5"
+                    value={profileData.address.line1}
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        address: { ...prev.address, line1: e.target.value },
+                      }))
+                    }
+                    type="text"
+                  />
+                ) : (
+                  profileData.address.line1
+                )}
+
+                <br />
+
+                {isEdit ? (
+                  <input
+                    className="bg-gray-100 rounded-xl max-w-fit pl-5 mt-1"
+                    value={profileData.address.line2}
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        address: { ...prev.address, line2: e.target.value },
+                      }))
+                    }
+                    type="text"
+                  />
+                ) : (
+                  profileData.address.line2
+                )}
               </p>
             </div>
 
             <div className="flex gap-1 pt-2">
-              <input type="checkbox" name="" id="" />
+              <input
+                checked={profileData.available}
+                onChange={() =>
+                  isEdit &&
+                  setProfileData((prev) => ({
+                    ...prev,
+                    available: !prev.available,
+                  }))
+                }
+                type="checkbox"
+                name=""
+                id=""
+              />
               <label htmlFor="">Available</label>
             </div>
 
-            <button className="px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all">
-              Edit
-            </button>
+            {isEdit ? (
+              <button
+                onClick={() => setIsEdit(false)}
+                className="px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all"
+              >
+                Save
+              </button>
+            ) : (
+              <button
+                onClick={() => setIsEdit(true)}
+                className="px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all"
+              >
+                Edit
+              </button>
+            )}
           </div>
         </div>
       </div>
