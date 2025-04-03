8324597649401d0bca0d3346f64bf5a4f02a44ff
Aaron Art Asperilla <166904523+SeiynJie@users.noreply.github.com>
2025-04-03 11:25:58 +0800

    Admin/Doc - Doctor Logout
    `Navbar.jsx`
    - Will now logout

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { AdminContext } from "../context/AdminContext";
 import { useNavigate } from "react-router-dom";
+import { DoctorContext } from "../context/DoctorContext";
 
 const Navbar = () => {
   const { aToken, setAToken } = useContext(AdminContext);
+  const { dToken, setDToken } = useContext(DoctorContext);
 
   const navigate = useNavigate();
 
@@ -13,6 +15,11 @@ const Navbar = () => {
     navigate("/");
     aToken && setAToken("");
     aToken && localStorage.removeItem("aToken");
+    
+    navigate("/");
+    dToken && setDToken("");
+    dToken && localStorage.removeItem("dToken");
+
   };
 
   return (
